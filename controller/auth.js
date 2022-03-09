f862a74c781fb46e10301b3f208d5413780de0d0
tiagofredk <tiagofredk@gmail.com>
2022-03-09 11:21:03 +0100

    undefined

--- controller/auth.js
+++ controller/auth.js
@@ -71,7 +71,7 @@ async function adduser (req, res, next) {
         });
         await newUsercopy.save()
             .then(data => {
-                res.json(data, {message: "success"})
+                res.json(data)
                 console.log("data collected");
             })
 
