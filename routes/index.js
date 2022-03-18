eb67c699c7ab9e404bc99be9f97e481018d1007b
tiagofredk <tiagofredk@gmail.com>
2022-03-18 13:01:54 +0100

    undefined

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ router.post("/message", authController.message);
 router.get("/verifytoken", authController.verifytoken);
 router.post('/login', authController.login);
 router.get("/logout", authController.logout);
+router.post("/adduser", authController.adduser);
 
 // all routes that come after this middleware are protected
 // and can only be accessed if the user is logged in
 router.use(authenticate);
 
-router.post("/adduser", authController.adduser);
 router.get('/profile', profileController.profile);
 
 module.exports = router;
